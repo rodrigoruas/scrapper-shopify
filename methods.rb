c3408acdff566a2fc11181ba763954f8f92698a9
Rodrigo Ruas <rodrigoruas2@gmail.com>
2020-04-16 23:41:19 +0100

    Add Script

--- methods.rb
+++ methods.rb
@@ -8,28 +8,41 @@ def export_to_csv(array,csv_file,headers)
       end
     end
   end
-  
+
   def export_array_to_json(array,json_file)
     File.open(json_file,"w") do |f|
       f.puts JSON.pretty_generate(array)
     end
   end
-  
+
   def read_json(file_path)
     JSON.parse((open(file_path).read).to_json)
   end
-  
+
   def read_csv(csv_file)
-    CSV.open(csv_file, headers: :first_row).map(&:to_h)
+    CSV.open(csv_file, headers: :first_row, col_sep: ";").map(&:to_h)
   end
-  
-  # def create_html_file(url)
-  #   open("festivals_htmls1/#{page_number}.html", 'wb') do |file|
-  #     file << open(url).read
-  #   end
-  # end
-  
-#   def create_slugs(string)
-#     string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '').gsub("--","-")
-#   end
-  
+
+  def get_urls_from_csv(csv_file)
+    array = []
+    CSV.open(csv_file, headers: :first_row, col_sep: ";").each do |line|
+      data = line.to_h
+      array << data["website"]
+    end
+    array
+  end
+
+  def read_csv(csv_file)
+    # CSV.open(csv_file, headers: :first_row, col_sep: ";").map(&:to_h)
+    CSV.open(csv_file, headers: :first_row, col_sep: ";").map do |line|
+      data = line.to_h
+      array << data["website"]
+    end
+  end
+
+def create_html_file(url)
+  open("french_websites/#{url.split("/").last}.html", 'wb') do |file|
+    file << open(url).read
+  end
+end
+
--- run.rb
+++ run.rb
@@ -1,23 +1,33 @@
+require 'thread/pool'
+require 'httparty'
+require 'csv'
+require 'net/http'
+require 'openssl'
+require "open-uri"
 require_relative "methods"
-require "json"
-require "csv"
-require "byebug"
 
-array = []
-file = File.open('part2.json').read
-file.each_line do |line|
-    z = line.gsub(" :", "\"").gsub("=>", "\": ").gsub("{:", "{\"").gsub("\n","").gsub(", Inc","").gsub("\\n","")
-    z.gsub!("nil", "0") if z.include? "nil"
-    begin
-        array << JSON.parse(z) 
-    rescue => e
-        p line
-        p e
+data_file = "part1.csv"
+
+puts "Open CSV file"
+@links = get_urls_from_csv(data_file)
+
+puts "Reading links..."
+@links.first(100).each_with_index do |link, index|
+  url = "http://#{link}"
+  p index
+  begin
+    response = HTTParty.get(url)
+    if((response.body.include?('lang="fr"')) || (response.body.include?('"contentLanguage"="fr"')))
+      print 'Allez les bleus!'
+      create_html_file(url)
     end
+    p "Success!"
+  rescue => e
+    p "Erro:"
+    p e
+  end
 end
 
-headers = %w(number website website_ip_address web_hosting_company web_hosting_location 
-web_hosting_city world_site_popular_rating website_popularity ip_reverse_dns 
-top_level_hostname web_hosting_state dns_records record_update_time)
+puts "Exporting french links to CSV..."
 
-export_to_csv(array,"part2.csv",headers)
+export_to_csv(@french_links, export_file_name)

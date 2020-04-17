a9f9ccf79d77a36ef23d2f772c52a4859a839d8a
Rodrigo Ruas <rodrigoruas2@gmail.com>
2020-04-17 17:15:32 +0100

    Fix file ceation

--- methods.rb
+++ methods.rb
@@ -1,44 +1,23 @@
-def export_to_csv(array,csv_file,headers)
-    CSV.open(csv_file, "w",
-            :write_headers=> true,
-            :col_sep => ";",
-            :headers => headers) do |csv|
-      array.each do |hash|
-        csv << hash.values
-      end
+def get_urls_from_csv(csv_file)
+  array = []
+  CSV.open(csv_file, headers: :first_row, col_sep: ";").each do |line|
+    data = line.to_h
+    array << data["website"]
+  end
+  array
+end
+
+def export_to_csv(links, file_name)
+  CSV.open(file_name, "w") do |csv|
+    csv << ["#", "website"]
+    links.each_with_index do |link, index|
+      csv << [index, link]
     end
   end
-
-  def export_array_to_json(array,json_file)
-    File.open(json_file,"w") do |f|
-      f.puts JSON.pretty_generate(array)
-    end
-  end
-
-  def read_json(file_path)
-    JSON.parse((open(file_path).read).to_json)
-  end
-
-  def get_urls_from_csv(csv_file)
-    array = []
-    CSV.open(csv_file, headers: :first_row, col_sep: ";").each do |line|
-      data = line.to_h
-      array << data["website"]
-    end
-    array
-  end
-
-  def read_csv(csv_file)
-    # CSV.open(csv_file, headers: :first_row, col_sep: ";").map(&:to_h)
-    CSV.open(csv_file, headers: :first_row, col_sep: ";").map do |line|
-      data = line.to_h
-      array << data["website"]
-    end
-  end
+end
 
 def create_html_file(url)
   open("french_websites/#{url.split("/").last}.html", 'wb') do |file|
     file << open(url).read
   end
 end
-
--- run.rb
+++ run.rb
@@ -1,29 +1,35 @@
+require 'thread/pool'
 require 'httparty'
 require 'csv'
-require_relative "methods"
+require "open-uri"
+require_relative 'methods'
 
 data_file = "part1.csv"
 
 puts "Open CSV file"
 @links = get_urls_from_csv(data_file)
+puts "#{@links.count} links founded"
+@french_links = []
 
+# pool = Thread.pool(500)
 puts "Reading links..."
-@links.first(100).each_with_index do |link, index|
+@links.first(1000).each_with_index do |link, index|
   url = "http://#{link}"
-  p index
-  begin
-    response = HTTParty.get(url)
-    if((response.body.include?('lang="fr"')) || (response.body.include?('"contentLanguage"="fr"')))
-      print 'Allez les bleus!'
-      create_html_file(url)
+  # pool.process {
+    begin
+      response = HTTParty.get(url)
+      if((response.body.include?('lang="fr"')) || (response.body.include?('"contentLanguage"="fr"')))
+        print 'Allez les bleus!'
+        create_html_file(url)
+      end
+      p "Success!"
+    rescue => e
+      p e
     end
-    p "Success!"
-  rescue => e
-    p "Erro:"
-    p e
-  end
-end
+     # sleep(1)
+  # }
+ end
+# pool.shutdown
 
 puts "Exporting french links to CSV..."
 
-export_to_csv(@french_links, export_file_name)

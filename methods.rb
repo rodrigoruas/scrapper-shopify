3aa4998df6099b137a8199adddf86b274686e70d
Rodrigo Ruas <rodrigoruas2@gmail.com>
2020-04-20 08:45:44 +0100

    using pools again

--- methods.rb
+++ methods.rb
@@ -21,3 +21,45 @@ def create_html_file(url)
     file << open(url).read
   end
 end
+
+def parse_with_pool(start, finish)
+  pool = Thread.pool(500)
+  start_time = Time.now
+  @links[start..finish].each_with_index do |link, index|
+    pool.process {
+      url = "http://#{link}"
+      begin
+        response = HTTParty.get(url)
+        if((response.body.include?('lang="fr"')) || (response.body.include?('"contentLanguage"="fr"')))
+          print 'Allez les bleus!'
+          file = create_html_file(url)
+        end
+        p "Success!"
+      rescue => e
+        p e
+      end
+    }  
+  end
+  pool.shutdown
+  p "#{(Time.now- start_time).round(2)} seconds"
+end
+
+def parse_without_pool(start, finish)
+  start_time = Time.now
+  @links[start..finish].each_with_index do |link, index|
+    p "======================================================"
+    p "parsing: #{index}"
+    url = "http://#{link}"
+    begin
+      response = HTTParty.get(url)
+      if((response.body.include?('lang="fr"')) || (response.body.include?('"contentLanguage"="fr"')))
+        print 'Allez les bleus!'
+        create_html_file(url)
+      end
+      p "Success!"
+    rescue => e
+      p e
+    end
+  end
+  p "#{(Time.now- start_time).round(2)} seconds"
+end
--- run.rb
+++ run.rb
@@ -3,33 +3,17 @@ require 'httparty'
 require 'csv'
 require "open-uri"
 require_relative 'methods'
+require 'nokogiri'
+require 'byebug'
 
 data_file = "part1.csv"
 
 puts "Open CSV file"
 @links = get_urls_from_csv(data_file)
-puts "#{@links.count} links founded"
-@french_links = []
 
-# pool = Thread.pool(500)
-puts "Reading links..."
-@links.first(1000).each_with_index do |link, index|
-  url = "http://#{link}"
-  # pool.process {
-    begin
-      response = HTTParty.get(url)
-      if((response.body.include?('lang="fr"')) || (response.body.include?('"contentLanguage"="fr"')))
-        print 'Allez les bleus!'
-        create_html_file(url)
-      end
-      p "Success!"
-    rescue => e
-      p e
-    end
-     # sleep(1)
-  # }
- end
-# pool.shutdown
+# Write here the interval you want to parse:
 
-puts "Exporting french links to CSV..."
+parse_with_pool(0,500)
+
+puts "Goodbye!"
 
